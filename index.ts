8e8a02a11d9fe181ad6252ab8163ed6cb0ddf754
adryarhamara <heeyrhamara@gmail.com>
2022-11-25 12:01:09 -0300

    starting studies

---
+++ index.ts
@@ -0,0 +1,10 @@
+function greet(name){
+    return `Olá ${name}`; //concatenando a variavel
+}
+console.log(greet('Adrya Rhamara'));
+
+function age(idade){
+    return `21 ${idade}`; //concatenando a variavel
+}
+console.log(age('anos'));
+
