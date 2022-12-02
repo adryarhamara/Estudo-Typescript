577f66df0637a3c44c358eafcc52a99c6cbedee8
adryarhamara <heeyrhamara@gmail.com>
2022-12-01 22:54:26 -0300

    finalizing environment settings

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+function greet(name: string){
+    return `Olá ${name}`; //concatenando a variavel
+}
+console.log(greet('Adrya Rhamara'));
+
+function age(idade:number){
+    return `anos  ${idade}`; //concatenando a variavel
+}
+console.log(age(21));
+
+
